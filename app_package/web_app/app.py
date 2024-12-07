6f05fe883e7dddb50dd8024a9e69d840651c0d75
Pamindu Janandith <pamindu.janandith@gmail.com>
2024-12-07 15:58:36 +0530

    0.02

--- app_package/web_app/app.py
+++ app_package/web_app/app.py
@@ -10,6 +10,31 @@ from utilities import get_data_frame
 frame = get_data_frame()
 countries = frame['Country'].unique()
 
+def region_selection_by_country(frame, country):
+    df = pd.DataFrame(frame)
+    df = df[df['Country'] == country]
+    return df
+
+def wine_style_selection_by_country_and_region(frame, country, region):
+    df = pd.DataFrame(frame)
+    df = df[df['Country'] == country]
+    df = df[df['Region'].str.contains(region, na=False)]
+    return df
+
+def df_selection_by_country_and_region_and_wine_style(frame, country, region, wine_style):
+    df = pd.DataFrame(frame)
+    df = df[df['Country'] == country]
+    df = df[df['Region'].str.contains(region, na=False)]
+    df = df[df['Wine style'] == wine_style]
+    return df
+
+df_of_one_region = region_selection_by_country(frame, countries[0])
+regions = df_of_one_region['Region'].str.split(' / ').apply(lambda x: x[1] if len(x) > 1 else None).unique()
+
+df_of_one_region_one_wine_style = wine_style_selection_by_country_and_region(frame, countries[0], regions[0])
+wine_styles = df_of_one_region_one_wine_style['Wine style'].unique()
+wine_styles = [style for style in wine_styles if pd.notna(style)]
+
 external_stylesheets = [
     {
         "href": (
@@ -48,34 +73,57 @@ app.layout = html.Div(
                         dcc.Dropdown(
                             id="country-filter",
                             options=[
-                                {"label": country, "value": country}
+                                {
+                                    "label": country, 
+                                    "value": country
+                                }
                                 for country in countries
                             ],
-                            value="USA",
+                            value=countries[0],
                             clearable=False,
                             className="dropdown",
                         ),
                     ]
                 ),
-                # html.Div(
-                #     children=[
-                #         html.Div(children="Type", className="menu-title"),
-                #         dcc.Dropdown(
-                #             id="type-filter",
-                #             options=[
-                #                 {
-                #                     "label": avocado_type.title(),
-                #                     "value": avocado_type,
-                #                 }
-                #                 for avocado_type in avocado_types
-                #             ],
-                #             value="organic",
-                #             clearable=False,
-                #             searchable=False,
-                #             className="dropdown",
-                #         ),
-                #     ],
-                # ),
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {
+                                    "label": region,
+                                    "value": region,
+                                }
+                                for region in regions
+                            ],
+                            value = regions[0],
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Wine Style", className="menu-title"),
+                        dcc.Dropdown(
+                            id="wine-style-filter",
+                            options=[
+                                {
+                                    "label": wine_style,
+                                    "value": wine_style,
+                                }
+                                for wine_style in wine_styles
+                            ],
+                            value = wine_styles[0],
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+
                 # html.Div(
                 #     children=[
                 #         html.Div(
@@ -95,13 +143,13 @@ app.layout = html.Div(
         ),
         html.Div(
             children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id="price-chart",
-                        config={"displayModeBar": False},
-                    ),
-                    className="card",
-                ),
+                # html.Div(
+                #     children=dcc.Graph(
+                #         id="price-chart",
+                #         config={"displayModeBar": False},
+                #     ),
+                #     className="card",
+                # ),
                 html.Div(
                     children=dcc.Graph(
                         id="BTSA-chart",
@@ -115,36 +163,39 @@ app.layout = html.Div(
     ]
 )
 
-
 @app.callback(
-    Output("price-chart", "figure"),
+    # Output("price-chart", "figure"),
     Output("BTSA-chart", "figure"),
     Input("country-filter", "value"),
+    Input("region-filter", "value"),
+    Input("wine-style-filter", "value"),
 )
-def update_charts(country):
-    filtered_data = frame.query(
-        "Country == @country"
-    )
-    price_chart_figure = {
-        "data": [
-            {
-                "x": filtered_data["Name"],
-                "y": filtered_data["Price"],
-                "type": "lines",
-                "hovertemplate": "$%{y:.2f}<extra></extra>",
-            },
-        ],
-        "layout": {
-            "title": {
-                "text": "Wine Price",
-                "x": 0.05,
-                "xanchor": "left",
-            },
-            "xaxis": {"fixedrange": True},
-            "yaxis": {"tickprefix": "$", "fixedrange": True},
-            "colorway": ["#17B897"],
-        },
-    }
+def update_charts(country, region, wine_style):
+    # filtered_data = frame.query(
+    #     "Country == @country"
+    # )
+    filtered_data = df_selection_by_country_and_region_and_wine_style(frame, country, region, wine_style)
+
+    # price_chart_figure = {
+    #     "data": [
+    #         {
+    #             "x": filtered_data["Name"],
+    #             "y": filtered_data["Price"],
+    #             "type": "lines",
+    #             "hovertemplate": "$%{y:.2f}<extra></extra>",
+    #         },
+    #     ],
+    #     "layout": {
+    #         "title": {
+    #             "text": "Wine Price",
+    #             "x": 0.05,
+    #             "xanchor": "left",
+    #         },
+    #         "xaxis": {"fixedrange": True},
+    #         "yaxis": {"tickprefix": "$", "fixedrange": True},
+    #         "colorway": ["#17B897"],
+    #     },
+    # }
 
     BTSA_chart_figure = {
         'data': [
@@ -157,7 +208,7 @@ def update_charts(country):
             'title': 'Wine Data Visualization'
         }
     }
-    return price_chart_figure, BTSA_chart_figure
+    return  BTSA_chart_figure #, price_chart_figure
 
 def get_app():
     return app
